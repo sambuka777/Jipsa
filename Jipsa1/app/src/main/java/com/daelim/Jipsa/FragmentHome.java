b445512b6d81610f9a479efbd20fe1731a63b31b
jjjang <wnscjf2wnscj@gmail.com>
2021-05-21 22:35:37 +0900

    태형이가 슬라드 한것 중간(디자인 바꾸는중)

--- Jipsa1/app/src/main/java/com/daelim/Jipsa/FragmentHome.java
+++ Jipsa1/app/src/main/java/com/daelim/Jipsa/FragmentHome.java
@@ -14,7 +14,12 @@ import android.widget.ViewFlipper;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.ViewCompat;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
+import androidx.viewpager2.widget.ViewPager2;
+
+import me.relex.circleindicator.CircleIndicator3;
 
 public class FragmentHome extends Fragment {
 
@@ -23,6 +28,10 @@ public class FragmentHome extends Fragment {
     }
 
     private View view;
+    private ViewPager2 mPager;
+    private FragmentStateAdapter pagerAdapter;
+    private int num_page = 4;
+    private CircleIndicator3 mIndicator;
 
     ImageView ViewPager;
     TextView TvComuTitle, TvComuList1, TvComuList2, TvComuList3, TvComuList4, TvLostAnimal;
@@ -45,6 +54,57 @@ public class FragmentHome extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_home, container, false);
+
+        //ViewPager2
+        mPager = view.findViewById(R.id.ViewPager_lost);
+        //Adapter
+        pagerAdapter = new MyAdapter(this, num_page);
+        mPager.setAdapter(pagerAdapter);
+        //Indicator
+        mIndicator = view.findViewById(R.id.indicator);
+        mIndicator.setViewPager(mPager);
+        mIndicator.createIndicators(num_page,0);
+        //ViewPager Setting
+        mPager.setOrientation(ViewPager2.ORIENTATION_HORIZONTAL);
+        mPager.setCurrentItem(1000);
+        mPager.setOffscreenPageLimit(3);
+
+        mPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+                super.onPageScrolled(position, positionOffset, positionOffsetPixels);
+                if (positionOffsetPixels == 0) {
+                    mPager.setCurrentItem(position);
+                }
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                super.onPageSelected(position);
+                mIndicator.animatePageSelected(position % num_page);
+            }
+        });
+
+        final float pageMargin= getResources().getDimensionPixelOffset(R.dimen.pageMargin);
+        final float pageOffset = getResources().getDimensionPixelOffset(R.dimen.offset);
+
+        mPager.setPageTransformer(new ViewPager2.PageTransformer() {
+            @Override
+            public void transformPage(@NonNull View page, float position) {
+                float myOffset = position * -(2 * pageOffset + pageMargin);
+                if (mPager.getOrientation() == ViewPager2.ORIENTATION_HORIZONTAL) {
+                    if (ViewCompat.getLayoutDirection(mPager) == ViewCompat.LAYOUT_DIRECTION_RTL) {
+                        page.setTranslationX(-myOffset);
+                    }
+                    else {
+                        page.setTranslationX(myOffset);
+                    }
+                }
+                else {
+                    page.setTranslationY(myOffset);
+                }
+            }
+        });
 
         BtnComuMore = view.findViewById(R.id.btn_ComuMore);
         BtnComuMore.setOnClickListener(new View.OnClickListener() {
