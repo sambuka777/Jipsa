3a255d36004eca29368d464701414e358b1574ea
eremi <barbara0419@nave.com>
2021-05-27 21:51:37 +0900

    lostwrite 화면 완료

--- Jipsa1/app/src/main/java/com/daelim/Jipsa/LostFindPet.java
+++ Jipsa1/app/src/main/java/com/daelim/Jipsa/LostFindPet.java
@@ -1,16 +1,28 @@
 package com.daelim.Jipsa;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.provider.Settings;
+import android.util.Log;
 import android.view.View;
-import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.RadioGroup;
+import android.widget.RelativeLayout;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -25,20 +37,46 @@ public class LostFindPet extends AppCompatActivity implements OnMapReadyCallback
     String id;
     ImageButton close_btn;
 
+    LocationManager locationManager;
     GoogleMap mMap;
+    double mLatitude, mLongitude;
+
+
 
     @Override
-    protected void onCreate(@Nullable Bundle savedInstanceState){
+    protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
         setContentView(R.layout.lost_write);
 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map_location);
-        mapFragment.getMapAsync(this);
-
         Intent intent = getIntent();
+
+        // 신고, 발견에 따른 값 세팅
         TextView title = (TextView)findViewById(R.id.write_title);
         title.setText("펫 " + intent.getStringExtra("text")+"신고");
 
+        TextView P_gps = (TextView)findViewById(R.id.pet_gps);
+        P_gps.setText("펫이 " + intent.getStringExtra("text")+"된 위치가 현재 위치와 같은가요?");
+
+        // 지도 숨기기
+        RelativeLayout map_location = (RelativeLayout)findViewById(R.id.map);
+        map_location.setVisibility(View.GONE);
+
+        //지도 보여주기 숨기기 이벤트
+        RadioGroup R_map = (RadioGroup)findViewById(R.id.radioGroup2);
+        R_map.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                if(checkedId == R.id.gps_no){
+                    map_location.setVisibility(View.VISIBLE);
+                }else if(checkedId == R.id.gps_yes){
+                    map_location.setVisibility(View.GONE);
+                }
+            }
+        });
+
+
+
+        //취소버튼 클릭시
         close_btn = findViewById(R.id.btn_close);
         close_btn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -52,11 +90,97 @@ public class LostFindPet extends AppCompatActivity implements OnMapReadyCallback
 
     }
 
+    //권한 요청후 응답 콜백
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        //ACCESS_COARSE_LOCATION 권한
+        if(requestCode==1){
+            //권한받음
+            if(grantResults.length>0 && grantResults[0]==PackageManager.PERMISSION_GRANTED){
+                requestMyLocation();
+            }
+            //권한못받음
+            else{
+                Toast.makeText(this, "권한없음", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+
+    //나의 위치 요청
+    public void requestMyLocation(){
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            return;
+        }
+        //요청
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 100, 10, locationListener);
+    }
+
+    //위치정보 구하기 리스너
+    LocationListener locationListener = new LocationListener() {
+        @Override
+        public void onLocationChanged(Location location) {
+            if(ContextCompat.checkSelfPermission(LostFindPet.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                    ContextCompat.checkSelfPermission(LostFindPet.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+                return;
+            }
+            //나의 위치를 한번만 가져오기 위해
+            locationManager.removeUpdates(locationListener);
+
+            //위도 경도
+            mLatitude = location.getLatitude();   //위도
+            mLongitude = location.getLongitude(); //경도
+
+            System.out.printf(String.valueOf(mLatitude),mLongitude);
+            //맵생성
+            SupportMapFragment mapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.map_location);
+            //콜백클래스 설정
+            mapFragment.getMapAsync(LostFindPet.this);
+        }
+
+        @Override
+        public void onStatusChanged(String provider, int status, Bundle extras) { Log.d("gps", "onStatusChanged"); }
+
+        @Override
+        public void onProviderEnabled(String provider) { }
+
+        @Override
+        public void onProviderDisabled(String provider) { }
+    };
+
     @Override
     public void onMapReady(GoogleMap googleMap) {
-        googleMap.addMarker(new MarkerOptions()
-                .position(new LatLng(0, 0))
-                .title("Marker"));
+        //LocationManager
+        locationManager = (LocationManager)getSystemService(LOCATION_SERVICE);
+
+
+        //GPS가 켜져있는지 체크
+        if(!locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER)){
+            //GPS 설정화면으로 이동
+            Intent intent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+            intent.addCategory(Intent.CATEGORY_DEFAULT);
+            startActivity(intent);
+            finish();
+        }
+
+        //권한이 없는 경우
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED ||
+                ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED){
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION , Manifest.permission.ACCESS_FINE_LOCATION} , 1);
+
+            setDefaultLocation();
+        }
+        //권한이 있는 경우
+        else{
+            requestMyLocation();
+        }
+
+        LatLng position = new LatLng(mLatitude , mLongitude);
+
+        //화면중앙의 위치와 카메라 줌비율
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
+
     }
 
     public void setDefaultLocation() {
