e198d2518be86a97275b9f1a66ed7bd461a1c4e2
Kwon <kwon844@email.daelim.ac.kr>
2021-05-31 00:23:38 +0900

    commit

--- Jipsa1/app/src/main/java/com/daelim/Jipsa/FragmentQnAPre.java
+++ Jipsa1/app/src/main/java/com/daelim/Jipsa/FragmentQnAPre.java
@@ -3,6 +3,7 @@ package com.daelim.Jipsa;
 import android.content.Context;
 import android.os.Bundle;
 import android.os.ParcelUuid;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -15,7 +16,16 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class FragmentQnAPre extends Fragment {
 
@@ -23,6 +33,9 @@ public class FragmentQnAPre extends Fragment {
     private MainActivity mainActivity;
 
     ImageButton ImgBtnQpBack, ImgBtnQpWrite;
+    private static final String TAG = "FragmentCommu";
+    FirebaseFirestore db;
+
 
     public void onAttach(Context context){
         super.onAttach(context);
@@ -50,7 +63,28 @@ public class FragmentQnAPre extends Fragment {
         qnapres.add(new QnApre("고구마호박 맛있네요.", "2021-05-30"));
         qnapres.add(new QnApre("어머니 호박~! 고구마요~", "2021-05-30"));
         qnapres.add(new QnApre("호.박.고.구.마 호박고구마~!~!~!", "2021-05-30"));
+        //
+        db = FirebaseFirestore.getInstance();
+//        CollectionReference citiesRef = db.collection("commity");
+//        db.collection("commity").get;
+        db.collection("commity").orderBy("date", Query.Direction.DESCENDING).get().addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                if (task.isSuccessful()) {
+                    for (QueryDocumentSnapshot document : task.getResult()) {
+                        Log.d(TAG, document.getId() + " => " + document.getData());
 
+                        Date from = new Date(document.getTimestamp("date").toDate().getTime());
+                        SimpleDateFormat transFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+                        String to = transFormat.format(from);
+                        System.out.println(to);
+                    }
+
+                } else {
+                    Log.w(TAG, "Error getting documents.", task.getException());
+                }
+            }
+        });
         ListQp = view.findViewById(R.id.list_Qp);
         qnapreAdapter = new QnApreAdapter(getContext(), qnapres);
         ListQp.setAdapter(qnapreAdapter);
