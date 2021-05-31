739a6204a8035d453e89303de0a6f908b5fa08b8
김태형 <xogud000925@gmail.com>
2021-05-31 23:30:04 +0900

    Auto stash before merge of "main" and "origin/main"
    채팅 리스트뷰화

--- Jipsa1/app/src/main/java/com/daelim/Jipsa/FragmentChat.java
+++ Jipsa1/app/src/main/java/com/daelim/Jipsa/FragmentChat.java
@@ -7,8 +7,11 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.Toast;
 
@@ -22,12 +25,15 @@ import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class FragmentChat extends Fragment {
 
     private View view;
-    Button cp_btn1,cp_btn2;
+    ImageButton ImgbtnChatBack, ImgbtnChatSerach;
     MainActivity mainActivity;
-    LinearLayout chat1,chat2;
+    LinearLayout chat1;
     String id;
     public void onAttach(Context context){
         super.onAttach(context);
@@ -39,7 +45,9 @@ public class FragmentChat extends Fragment {
         mainActivity = null;
     }
 
-
+    ArrayList<Chat> chats;
+    ListView ListChat;
+    private static ChattingAdapter chattingAdapter;
 
 
     @Nullable
@@ -47,38 +55,84 @@ public class FragmentChat extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_chat, container, false);
         System.out.println(id);
-        chat1 = view.findViewById(R.id.chat_1);
-        chat1.setOnClickListener(new View.OnClickListener() {
+
+        chats = new ArrayList<>();
+        chats.add(new Chat("김태형", "채팅내역1", "2021-05-31"));
+        chats.add(new Chat("장준철", "채팅내역2", "2021-05-30"));
+        chats.add(new Chat("김찬우", "채팅내역3", "2021-05-29"));
+        chats.add(new Chat("권대웅", "채팅내역4", "2021-05-28"));
+        chats.add(new Chat("실종자", "채팅내역5", "2021-05-27"));
+
+        ListChat = view.findViewById(R.id.list_Chat);
+        chattingAdapter = new ChattingAdapter(getContext(), chats);
+
+        ListChat.setAdapter(chattingAdapter);
+        ListChat.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-                //mainActivity.setFrag(5);
-                FirebaseFirestore db;
-                db = FirebaseFirestore.getInstance();
-
-                DocumentReference docRef = db.collection("members").document(id);
-                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-                    @Override
-                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-                        if (task.isSuccessful()) {
-                            DocumentSnapshot document = task.getResult();
-                            if (document.exists()) {
-                                Log.d("TAG", "DocumentSnapshot data: " + document.getData());
-                                System.out.println(document.get("name").toString());
-                                Intent intent = new Intent(getActivity(), Chatroom2.class);
-                                intent.putExtra("id",document.get("name").toString());
-
-                                startActivity(intent);
-                            } else {
-                                Log.d("TAG", "No such document");
-                            }
-                        } else {
-                            Log.d("TAG", "get failed with ", task.getException());
-                        }
-                    }
-                });
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                String selectedItem = view.findViewById(R.id.ch_name1).getTag().toString();
+                Toast.makeText(getContext(), "Clicked: " + position +" " + selectedItem, Toast.LENGTH_SHORT).show();
             }
+
+//            FirebaseFirestore db;
+//            db = FirebaseFirestore.getInstance();
+//
+//            DocumentReference docRef = db.collection("members").document(id);
+//                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                @Override
+//                public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                    if (task.isSuccessful()) {
+//                        DocumentSnapshot document = task.getResult();
+//                        if (document.exists()) {
+//                            Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+//                            System.out.println(document.get("name").toString());
+//                            Intent intent = new Intent(getActivity(), Chatroom2.class);
+//                            intent.putExtra("id",document.get("name").toString());
+//
+//                            startActivity(intent);
+//                        } else {
+//                            Log.d("TAG", "No such document");
+//                        }
+//                    } else {
+//                        Log.d("TAG", "get failed with ", task.getException());
+//                    }
+//                }
+//            });
+
         });
+
+//        chat1 = view.findViewById(R.id.chat_1);
+//        chat1.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                //mainActivity.setFrag(5);
+//                FirebaseFirestore db;
+//                db = FirebaseFirestore.getInstance();
+//
+//                DocumentReference docRef = db.collection("members").document(id);
+//                docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+//                    @Override
+//                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+//                        if (task.isSuccessful()) {
+//                            DocumentSnapshot document = task.getResult();
+//                            if (document.exists()) {
+//                                Log.d("TAG", "DocumentSnapshot data: " + document.getData());
+//                                System.out.println(document.get("name").toString());
+//                                Intent intent = new Intent(getActivity(), Chatroom2.class);
+//                                intent.putExtra("id",document.get("name").toString());
+//
+//                                startActivity(intent);
+//                            } else {
+//                                Log.d("TAG", "No such document");
+//                            }
+//                        } else {
+//                            Log.d("TAG", "get failed with ", task.getException());
+//                        }
+//                    }
+//                });
+//
+//            }
+//        });
 
 //        chat2 = view.findViewById(R.id.chat_2);
 //        chat2.setOnClickListener(new View.OnClickListener() {
@@ -89,7 +143,33 @@ public class FragmentChat extends Fragment {
 //        });
 
 
-        return view;    }
+        return view;
+    }
+
+    class Chat {
+        private String chatName;
+        private String chatContents;
+        private String chatDate;
+
+        public Chat(String chatName, String chatContents, String chatDate) {
+            this.chatName = chatName;
+            this.chatContents = chatContents;
+            this.chatDate = chatDate;
+        }
+
+        public String getChatName() {
+            return chatName;
+        }
+
+        public String getChatContents() {
+            return chatContents;
+        }
+
+        public String getChatDate() {
+            return chatDate;
+        }
+    }
+
     public void set_id(String id){
         this.id = id;
     }
